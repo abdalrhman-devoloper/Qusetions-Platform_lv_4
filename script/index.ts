ddc156ec36a795a90590ed8608615dec726faa74
Abdalrhman8000 <75347437+Abdalrhman8000@users.noreply.github.com>
2022-05-24 09:56:47 +0200

    add typescript

--- script/index.ts
+++ script/index.ts
@@ -1,265 +1,304 @@
 //============= Set || Nodes || ==================> 
-var audio = document.querySelector('.audio');
-var start_quest = document.querySelector('#start_quest');
-var quset_cont = document.querySelector('.quset_cont');
-var quest_index = document.querySelector('.quest_index');
-var answer = document.querySelector('.answers');
-var quest_index_num = document.querySelector('.quest_index_num');
-var remove_2 = document.querySelector('.remove_2');
-var google = document.querySelector('.google');
-var answers = document.querySelectorAll('.answer');
-var audio_cotroll_img = document.querySelectorAll('.audio_cotroll img');
-var start = 0;
-var answer_data;
+const audio = document.querySelector('.audio') as HTMLAudioElement;
+const start_quest = document.querySelector('#start_quest') as HTMLElement;
+const quset_cont = document.querySelector('.quset_cont') as HTMLElement;
+const quest_index = document.querySelector('.quest_index') as HTMLElement;
+const answer = document.querySelector('.answers') as HTMLElement;
+const quest_index_num = document.querySelector('.quest_index_num') as HTMLElement;
+const remove_2 = document.querySelector('.remove_2') as HTMLElement;
+const google = document.querySelector('.google') as HTMLElement;
+const answers = document.querySelectorAll('.answer') as NodeListOf<HTMLElement>;
+const audio_cotroll_img = document.querySelectorAll('.audio_cotroll img') as NodeListOf<HTMLElement> ;
+let start = 0;
+let answer_data;
 //============= Set || Nodes || ==================>
+
+
+
+
+
 //======= Set || classes || =======>
-var DrawUi = /** @class */ (function () {
-    function DrawUi(data, index) {
+
+class DrawUi {
+    data:string;
+    index:number;
+    state:boolean;
+    constructor(data:string, index:number) {
         this.data = data;
         this.index = index;
         this.state = true;
     }
+
     //======= Draw || All ||  Content || Ui =======>
-    DrawUi.prototype.draw = function () {
-        var _this = this;
+    draw() {
         if (this.data != undefined && this.index != undefined) {
-            fetch(this.data).then(function (response) { return response.json(); }).then(function (values) {
-                _this.stop_Quiz(values);
-                if (_this.state) {
-                    _this.draw_Quest(values);
-                    _this.draw_Quest_line(values);
+            fetch(this.data).then(response => response.json()).then(values => {
+                this.stop_Quiz(values);
+                if (this.state) {
+                    this.draw_Quest(values);
+                    this.draw_Quest_line(values);
                 }
-            });
-        }
-        else {
+            })
+        } else {
             console.warn('please Enter data src and index of start');
         }
-    };
+    }
     //======= Draw || All ||  Content || Ui =======>
+
+
     //======= Draw || Questions || Answers =======>
-    DrawUi.prototype.draw_Quest = function (values) {
-        var _this = this;
-        var recust = ['a', 'b', 'c', 'd'];
+    draw_Quest(values:any) {
+        let recust = ['a', 'b', 'c', 'd'];
         quset_cont.innerText = values[this.index].quest;
-        answers.forEach(function (ele, index) {
-            ele.innerText = values[_this.index][recust[index]];
+        answers.forEach((ele, index) => {
+            ele.innerText = values[this.index][recust[index]];
         });
-        quest_index.innerText = "Question ".concat(this.index + 1);
-    };
+        quest_index.innerText = `Question ${this.index + 1}`;
+    }
     //======= Draw || Questions || Answers =======>
+
+
     //======= Draw || Questions || Lines =======>
-    DrawUi.prototype.draw_Quest_line = function (values) {
-        var quest_index_num_elements = quest_index_num.children;
+    draw_Quest_line(values:NodeListOf<HTMLElement>) {
+        const quest_index_num_elements = quest_index_num.children;
         if (quest_index_num_elements.length == 0) {
-            values.forEach(function (ele, index) {
-                quest_index_num.innerHTML += "<p>".concat(index + 1, "</p>");
-            });
+            values.forEach((ele:HTMLElement, index:number) => {
+                quest_index_num.innerHTML += `<p>${index + 1}</p>`;
+            })
         }
-        Array.from(quest_index_num_elements).forEach(function (ele) {
+        Array.from(quest_index_num_elements).forEach(ele => {
             ele.classList.remove('active_point_quest');
-        });
+        })
         quest_index_num_elements[this.index].classList.add('active_point_quest');
-    };
+    }
     //======= Draw || Questions || Lines =======>
+
+
     //======= Check || Index || Stop "Quiz" =======>
-    DrawUi.prototype.stop_Quiz = function (values) {
+
+    stop_Quiz(values:[]) {
         if (this.index > values.length - 1) {
             this.state = false;
         }
-    };
-    return DrawUi;
-}());
-var Checker = /** @class */ (function () {
-    function Checker() {
+    }
+    //======= Check || Index || Stop "Quiz" =======>
+
+}
+
+class Checker {
+    darwer:DrawUi;
+    quest_index_num:HTMLCollection;
+    constructor() {
         this.darwer = new DrawUi('./auth/data.json', start);
         this.quest_index_num = quest_index_num.children;
     }
+
     //======= Check || All ||  Content || Answers =======>
-    Checker.prototype.main_checker = function (ele) {
-        var _this = this;
-        var http = new XMLHttpRequest();
-        http.onload = function () {
+    main_checker(ele:HTMLElement ) {
+        const http = new XMLHttpRequest();
+        http.onload = () => {
             if (http.status == 200) {
-                var data = JSON.parse(http.responseText);
-                _this.answer_check(data);
-                _this.line_check(data, ele);
-                _this.Repeat(data);
+                const data = JSON.parse(http.responseText);
+                this.answer_check(data);
+                this.line_check(data, ele);
+                this.Repeat(data);
             }
-        };
+        }
         http.open('GET', this.darwer.data, true);
         http.send();
-    };
+    }
     //======= Check || All ||  Content || Answers =======>
+
     //======= Set|| All ||  Content || Answers =======>
-    Checker.prototype.set_answers = function (data, state, statement) {
-        var _this = this;
+    set_answers(data:any, state:boolean, statement:boolean) {
         if (state) {
-            answers.forEach(function (ele) {
-                if (ele.getAttribute('answer') != data[_this.darwer.index].answer) {
+            answers.forEach(ele => {
+                if (ele.getAttribute('answer') != data[this.darwer.index].answer) {
                     ele.classList.add('falsey_answer');
-                }
-                else {
+                } else {
                     ele.classList.add('correct_answer');
                 }
                 if (statement) {
-                    setTimeout(function () {
+                    setTimeout(() => {
                         ele.classList.remove('falsey_answer');
                         ele.classList.remove('correct_answer');
                     }, 1200);
                 }
-            });
+            })
         }
-    };
+    }
     //======= Set|| All ||  Content || Answers =======>
+
+
     //======= Set || All ||  Styles || Answers =======>
-    Checker.prototype.answer_check = function (data) {
-        var _this = this;
+    answer_check(data:string) {
         if (this.darwer.index < data.length && this.darwer.index != data.length - 1) {
             this.set_answers(data, true, true);
+        } else {
+            this.set_answers(data, true, false)
         }
-        else {
-            this.set_answers(data, true, false);
-        }
-        setTimeout(function () {
-            _this.darwer.draw();
+        setTimeout(() => {
+            this.darwer.draw();
         }, 1200);
-    };
+    }
     //======= Set || All ||  Styles || Answers =======>
+
+
     //======= Set || All ||  Styles || Answers || Lines =======>
-    Checker.prototype.line_check = function (data, ele) {
+
+    line_check(data:any, ele:HTMLElement) {
         if (this.darwer.index < data.length) {
             if (ele.getAttribute('answer') == data[this.darwer.index].answer) {
                 this.quest_index_num[this.darwer.index].classList.add('correct_answer');
                 this.darwer.index++;
-            }
-            else {
+            } else {
                 this.quest_index_num[this.darwer.index].classList.add('falsey_answer');
                 this.darwer.index++;
             }
         }
-    };
+    }
     //======= Set || All ||  Styles || Answers || Lines =======>
+
     //======= Reapet_Quiz || =======>
-    Checker.prototype.Repeat = function (data) {
+    Repeat(data:[]) {
         if (this.darwer.index == data.length) {
             start_quest.classList.remove('hidde_animate_btn');
             start_quest.innerText = 'Repeat';
-            start_quest.addEventListener('click', function () {
+            start_quest.addEventListener('click', () => {
                 location.reload();
-            });
+            })
         }
-    };
-    return Checker;
-}());
-var Delet = /** @class */ (function () {
-    function Delet(ele, data, index) {
+    }
+    //======= Reapet_Quiz || =======>
+}
+
+class Delet {
+    ele;
+    data;
+    index;
+    constructor(ele:HTMLElement, data:string, index:number ) {
         this.ele = ele;
         this.data = data;
         this.index = index;
     }
-    Delet.prototype.swper = function () {
+
+    swper() {
         if (this.ele.innerText == '0.5') {
             this.get_data();
+        } else {
+            window.open('https://google.com/')
         }
-        else {
-            window.open('https://google.com/');
-        }
-    };
-    Delet.prototype.get_data = function () {
-        var _this = this;
-        fetch(this.data).then(function (response) { return response.json(); }).then(function (data) {
-            _this["delete"](data);
-        });
-    };
-    Delet.prototype["delete"] = function (data) {
-        var _this = this;
+    }
+
+
+    get_data() {
+        fetch(this.data).then(response => response.json()).then(data => {
+            this.delete(data);
+        })
+    }
+
+
+    delete(data:any) {
         if (quset_cont.innerText != '') {
-            var falsey_data_1 = [];
-            Array.from(answers).filter(function (ele, index) {
-                if (ele.getAttribute('answer') != data[_this.index].answer) {
-                    falsey_data_1.push(index);
+            let falsey_data:number[] = [];
+            Array.from(answers).filter((ele, index) => {
+                if (ele.getAttribute('answer') != data[this.index].answer) {
+                    falsey_data.push(index);
                 }
-            });
-            answers[falsey_data_1[0]].innerText = '';
-            answers[falsey_data_1[1]].innerText = '';
+            })
+            answers[falsey_data[0]].innerText = '';
+            answers[falsey_data[1]].innerText = '';
         }
-    };
-    return Delet;
-}());
+    }
+}
 //======= Set || classes || =======>
+
+
+
 //======= Start || Quiz || App =======>
 start_quest.addEventListener('click', start_app);
-function start_app(e) {
+function start_app(e:any) {
+
     //======= Audio || Controls =======>
-    audio.play();
+    audio.play()
     audio_cotroll_img[1].classList.remove('active');
     audio_cotroll_img[0].classList.add('active');
     //======= Audio || Controls =======>
+
     //======= Draw || Ui || Class =======>
-    var play = new DrawUi('./auth/data.json', start);
+    const play = new DrawUi('./auth/data.json', start);
     play.draw();
     //======= Draw || Ui || Class =======>
+
     //======= Checker || In || Chossen =======>
-    var chek = new Checker();
-    answers.forEach(function (ele) {
-        ele.addEventListener('click', function (e) {
-            chek.main_checker(e.target);
-        });
-    });
+    const chek = new Checker();
+    answers.forEach(ele => {
+        ele.addEventListener('click', (e) => {
+            chek.main_checker( e.target as HTMLElement);
+        })
+    })
     //======= Checker || In || Chossen =======>
+
     //======= Delet || Button || And Event =======>
     e.target.classList.add("hidde_animate_btn");
     e.target.removeEventListener('click', start_app);
     //======= Delet || Button || And Event =======>
+
+
+
     //======= Remove || Two || Answers =======>
-    remove_2.addEventListener("click", function (e) {
-        var deleter = new Delet(e.target, chek.darwer.data, chek.darwer.index);
+    remove_2.addEventListener("click", (e) => {
+        const deleter = new Delet(e.target as HTMLElement , chek.darwer.data,chek.darwer.index );
         deleter.swper();
-    });
+    })
     //======= Remove || Two || Answers =======>
+
     //======= Searcher || Event =======>
-    google.addEventListener("click", function (e) {
-        var deleter = new Delet(e.target, chek.darwer.data, chek.darwer.index);
+    google.addEventListener("click", (e) => {
+        const deleter = new Delet(e.target as HTMLElement , chek.darwer.data,chek.darwer.index );
         deleter.swper();
-    });
+    })
     //======= Searcher || Event =======>
+
     //===== Simple || Animate ========>
     quest_index_num.style.padding = '0';
 }
 //======= Start || Quiz || App =======>
+
+
 //======= Audio || Controller || App =======>
-audio_cotroll_img.forEach(function (ele) {
-    ele.addEventListener('click', function () {
+audio_cotroll_img.forEach(ele => {
+    ele.addEventListener('click', () => {
         if (audio.paused) {
             audio_cotroll_img[1].classList.remove('active');
             audio_cotroll_img[0].classList.add('active');
             audio.play();
-        }
-        else if (!audio.paused) {
+        } else if (!audio.paused) {
             audio_cotroll_img[1].classList.add('active');
             audio_cotroll_img[0].classList.remove('active');
             audio.pause();
         }
-    });
-});
+    })
+})
 //======= Audio || Controller || App =======>
+
+
 //========= Handel  || Hover  || Effect  || in  || Small  || Deveices ||  ============>
-window.addEventListener('resize', function () {
+window.addEventListener('resize', () => {
     if (window.innerWidth < 961) {
-        var hover = document.querySelectorAll('.hover');
-        hover.forEach(function (ele) {
+        const hover = document.querySelectorAll('.hover');
+        hover.forEach(ele => {
             ele.classList.add('hover_2');
             ele.classList.remove('hover');
-        });
+        })
         console.warn('resized to small delet hover');
-    }
-    else {
-        var hover_2 = document.querySelectorAll('.hover_2');
-        hover_2.forEach(function (ele) {
-            ele.classList.add('hover');
+    } else {
+        const hover_2 = document.querySelectorAll('.hover_2');
+        hover_2.forEach(ele => {
+            ele.classList.add('hover')
             ele.classList.remove('hover_2');
-        });
+        })
         console.warn('resized to Big add hover');
     }
-});
+})
 //========= Handel  || Hover  || Effect  || in  || Small  || Deveices ||  ============>
